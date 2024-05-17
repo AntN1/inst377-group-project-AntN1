96920d2efe662e9981e87449c2bf5471c7adb4b1
AntN1 <anega1@terpmail.umd.edu>
2024-05-17 15:34:10 -0400

    moved all javascript code to app.js file. update fetchQuote function to use async/await

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-function fetchquote() {
-    fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
-    .then(response => response.json())
-    .then(data => {
-        const quotetext = document.getelementbyid('quote-text');
-        quotetext.textcontent = data[0];
-    })
+async function fetchQuote() {
+  const api_url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+  const res = await fetch(api_url);
+  var data = await res.json();
+  return data;
 }
-document.addeventlistener('domcontentloaded', fetchquote);
+
+async function displayQuote() {
+  if (window.location.pathname === "/project.html") {
+    const data = await fetchQuote();
+    const quotetext = document.getElementById("quote-text");
+    quotetext.textContent = data[0];
+  } else {
+    return;
+  }
+}
+
+displayQuote();
